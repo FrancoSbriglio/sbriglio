ab31c7083e744d2c7040f40b1a0f7fdcb5d5f815
Franco Sbriglio <franco.sbriglio@gmail.com>
2019-10-29 15:44:32 -0300

    vehiculo

--- src/main/java/com/mycompany/myapp/repository/VehiculoRepository.java
+++ src/main/java/com/mycompany/myapp/repository/VehiculoRepository.java
@@ -1,4 +1,8 @@
 package com.mycompany.myapp.repository;
+
+import java.security.cert.PKIXRevocationChecker.Option;
+import java.util.Optional;
+
 import com.mycompany.myapp.domain.Vehiculo;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
@@ -10,5 +14,7 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface VehiculoRepository extends JpaRepository<Vehiculo, Long> {
+
+    Optional<Vehiculo> findByDominio(String dominio);
 
 }
--- src/main/java/com/mycompany/myapp/web/rest/VehiculoResource.java
+++ src/main/java/com/mycompany/myapp/web/rest/VehiculoResource.java
@@ -102,6 +102,15 @@ public class VehiculoResource {
         Optional<Vehiculo> vehiculo = vehiculoRepository.findById(id);
         return ResponseUtil.wrapOrNotFound(vehiculo);
     }
+   
+    @GetMapping("/vehiculos/dominio/{dominio}")
+    public ResponseEntity<Vehiculo> getVehiculo(@PathVariable String dominio) {
+        log.debug("REST request to get Vehiculo : {}", dominio);
+        Optional<Vehiculo> vehiculo = vehiculoRepository.findByDominio(dominio);
+        return ResponseUtil.wrapOrNotFound(vehiculo);
+    }
+
+    
 
     /**
      * {@code DELETE  /vehiculos/:id} : delete the "id" vehiculo.
